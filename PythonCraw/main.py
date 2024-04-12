68f63414316a746b279cae409a824f60e9f5d3ce
Duc <44010887+duckmak14@users.noreply.github.com>
2024-04-12 20:21:55 +0700

    add python bot

---
+++ PythonCraw/main.py
@@ -0,0 +1,26 @@
+import discord
+import json
+import pytz
+
+from datetime import datetime
+from discord.ext import commands
+
+
+intents = discord.Intents.all()
+intents.message_content = True
+bot = commands.Bot(command_prefix=".",intents=intents)
+BOTTOKEN = 'MTIyODAxMjEzMjI2NDU3OTExMg.GNJTFk.TjuW3Jpl4Ce9KfKoAMKb7nzBMdZcfzMY9pct3g' # token của bot được tạo
+@bot.event
+async def on_message(message): # Sự kiện khi gửi message
+    messageTime = message.created_at # Thời gian gửi messages 
+    current_time_utc = datetime.now(pytz.UTC)
+    time_difference = current_time_utc - messageTime # Thời gian tính từ lúc gửi đến lúc crawl về
+    print(time_difference)
+    
+@bot.command()
+async def hello(ctx):
+    print("command")
+    await ctx.send("hello, I am a discrod bot")
+    
+
+bot.run(BOTTOKEN)
