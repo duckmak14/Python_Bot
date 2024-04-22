d98e61ae5292ef6ee9009dbde82928fa58432246
Duc <44010887+duckmak14@users.noreply.github.com>
2024-04-22 09:51:07 +0700

    channel + text + url images

--- PythonCraw/main.py
+++ PythonCraw/main.py
@@ -1,26 +1,33 @@
 import discord
-import json
 import pytz
-
+import asyncio
 from datetime import datetime
 from discord.ext import commands
+import time
 
 
 intents = discord.Intents.all()
 intents.message_content = True
 bot = commands.Bot(command_prefix=".",intents=intents)
-BOTTOKEN = 'MTIyODAxMjEzMjI2NDU3OTExMg.GNJTFk.TjuW3Jpl4Ce9KfKoAMKb7nzBMdZcfzMY9pct3g' # token của bot được tạo
+BOTTOKEN = '' # token của bot được tạo
+
 @bot.event
 async def on_message(message): # Sự kiện khi gửi message
     messageTime = message.created_at # Thời gian gửi messages 
     current_time_utc = datetime.now(pytz.UTC)
     time_difference = current_time_utc - messageTime # Thời gian tính từ lúc gửi đến lúc crawl về
-    print(time_difference)
-    
+    #print(time_difference)
+    print(message.channel) # tên của channel
+    print(message.content) # text messages 
+    if message.attachments:
+        for attachment in message.attachments:
+            print(attachment.url)
+    # print(message.attachments[0].url)
+    await bot.process_commands(message)
+
 @bot.command()
 async def hello(ctx):
     print("command")
     await ctx.send("hello, I am a discrod bot")
     
-
 bot.run(BOTTOKEN)
