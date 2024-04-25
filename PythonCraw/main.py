c3bb2102afd5821477b3a59918ce40f8a7d8c963
Duc <44010887+duckmak14@users.noreply.github.com>
2024-04-25 10:42:13 +0700

    commid get messages with user session

--- PythonCraw/main.py
+++ PythonCraw/main.py
@@ -3,30 +3,22 @@ import pytz
 from datetime import datetime
 from discord.ext import commands
 
+bot = commands.Bot(command_prefix='.', self_bot=True)
+BOTTOKEN = ''
 
-intents = discord.Intents.all()
-intents.message_content = True
-bot = commands.Bot(command_prefix=".",intents=intents)
-BOTTOKEN = '' # token của bot được tạo
 
 @bot.event
 async def on_message(message): # Sự kiện khi gửi message
     messageTime = message.created_at # Thời gian gửi messages 
     current_time_utc = datetime.now(pytz.UTC)
     time_difference = current_time_utc - messageTime # Thời gian tính từ lúc gửi đến lúc crawl về
-    #print(time_difference)
+    print(time_difference)
     print(message.guild) # tên của server
     print(message.channel) # tên của channel
     print(message.content) # text messages 
     if message.attachments:
         for attachment in message.attachments:
             print(attachment.url)
-    # print(message.attachments[0].url)
     await bot.process_commands(message)
 
-@bot.command()
-async def hello(ctx):
-    print("command")
-    await ctx.send("hello, I am a discrod bot")
-    
 bot.run(BOTTOKEN)
